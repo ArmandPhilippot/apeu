e3ad5a39a11e39141ed29906021c4caac50fb00a
Armand Philippot <git@armand.philippot.eu>
2024-10-29 00:38:24 +0100

    feat: use our new components inside Layout

--- src/components/templates/layout/layout.test.ts
+++ src/components/templates/layout/layout.test.ts
@@ -1,6 +1,6 @@
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import type { ComponentProps } from "astro/types";
-import { beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import Layout from "./layout.astro";
 
 type LocalTestContext = {
@@ -10,6 +10,10 @@ type LocalTestContext = {
 describe("Layout", () => {
   beforeEach<LocalTestContext>(async (context) => {
     context.container = await AstroContainer.create();
+  });
+
+  afterEach<LocalTestContext>(() => {
+    vi.unstubAllEnvs();
   });
 
   it<LocalTestContext>("renders the website structure", async ({
@@ -46,5 +50,40 @@ describe("Layout", () => {
     expect.assertions(1);
 
     expect(result).toContain(body);
+  });
+
+  it<LocalTestContext>("can render a link to the design system in dev mode", async ({
+    container,
+  }) => {
+    vi.stubEnv("MODE", "development");
+
+    const props = {
+      title: "et ratione dolor",
+    } satisfies ComponentProps<typeof Layout>;
+    const result = await container.renderToString(Layout, {
+      props,
+    });
+
+    expect.assertions(1);
+
+    expect(result).toContain("/design-system");
+  });
+
+  it<LocalTestContext>("does not render a link to the design system in other modes than dev", async ({
+    container,
+  }) => {
+    vi.stubEnv("MODE", "production");
+    vi.stubEnv("DEV", false);
+
+    const props = {
+      title: "et ratione dolor",
+    } satisfies ComponentProps<typeof Layout>;
+    const result = await container.renderToString(Layout, {
+      props,
+    });
+
+    expect.assertions(1);
+
+    expect(result).not.toContain("/design-system");
   });
 });

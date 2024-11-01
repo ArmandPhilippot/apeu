fbd695a1967a964c567476d135f8166683dc9720
Armand Philippot <git@armand.philippot.eu>
2024-11-01 19:19:39 +0100

    feat: add more options to Box component

--- src/components/atoms/box/box.test.ts
+++ src/components/atoms/box/box.test.ts
@@ -55,6 +55,66 @@ describe("Box", () => {
     expect(result).toContain("centered");
   });
 
+  it<LocalTestContext>("can render a raised box", async ({ container }) => {
+    const props = {
+      elevation: "raised",
+    } satisfies ComponentProps<typeof Box<"div">>;
+    const body = "consequatur doloremque voluptas";
+    const result = await container.renderToString(Box, {
+      props,
+      slots: { default: body },
+    });
+
+    expect.assertions(1);
+
+    expect(result).toContain("raised");
+  });
+
+  it<LocalTestContext>("can render an elevated box", async ({ container }) => {
+    const props = {
+      elevation: "elevated",
+    } satisfies ComponentProps<typeof Box<"div">>;
+    const body = "consequatur doloremque voluptas";
+    const result = await container.renderToString(Box, {
+      props,
+      slots: { default: body },
+    });
+
+    expect.assertions(1);
+
+    expect(result).toContain("elevated");
+  });
+
+  it<LocalTestContext>("can render a floating box", async ({ container }) => {
+    const props = {
+      elevation: "floating",
+    } satisfies ComponentProps<typeof Box<"div">>;
+    const body = "consequatur doloremque voluptas";
+    const result = await container.renderToString(Box, {
+      props,
+      slots: { default: body },
+    });
+
+    expect.assertions(1);
+
+    expect(result).toContain("floating");
+  });
+
+  it<LocalTestContext>("can render a padded box", async ({ container }) => {
+    const props = {
+      isPadded: true,
+    } satisfies ComponentProps<typeof Box<"div">>;
+    const body = "consequatur doloremque voluptas";
+    const result = await container.renderToString(Box, {
+      props,
+      slots: { default: body },
+    });
+
+    expect.assertions(1);
+
+    expect(result).toContain("padded");
+  });
+
   it<LocalTestContext>("can render a box sized to prose", async ({
     container,
   }) => {
@@ -70,5 +130,35 @@ describe("Box", () => {
     expect.assertions(1);
 
     expect(result).toContain("prose");
+  });
+
+  it<LocalTestContext>("can render a rounded box", async ({ container }) => {
+    const props = {
+      isRounded: true,
+    } satisfies ComponentProps<typeof Box<"div">>;
+    const body = "consequatur doloremque voluptas";
+    const result = await container.renderToString(Box, {
+      props,
+      slots: { default: body },
+    });
+
+    expect.assertions(1);
+
+    expect(result).toContain("rounded");
+  });
+
+  it<LocalTestContext>("can render a spaced box", async ({ container }) => {
+    const props = {
+      isSpaced: true,
+    } satisfies ComponentProps<typeof Box<"div">>;
+    const body = "consequatur doloremque voluptas";
+    const result = await container.renderToString(Box, {
+      props,
+      slots: { default: body },
+    });
+
+    expect.assertions(1);
+
+    expect(result).toContain("spaced");
   });
 });
--- src/types/tokens.ts
+++ src/types/tokens.ts
@@ -1,4 +1,9 @@
 /**
+ * The elevations defined in `src/styles/variables/shadows.css`.
+ */
+export type Elevation = "raised" | "elevated" | "floating";
+
+/**
  * The filenames available in `src/assets/icons` directory.
  *
  * These tokens are meant to be used with `astro-icon`.

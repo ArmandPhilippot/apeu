94baf012d174ac8d1d681b5fbbc65b671042d75b
Armand Philippot <git@armand.philippot.eu>
2024-11-03 23:39:43 +0100

    feat: add SearchForm to Navbar

--- src/components/organisms/navbar/navbar.test.ts
+++ src/components/organisms/navbar/navbar.test.ts
@@ -16,15 +16,82 @@ describe("Navbar", () => {
     const props = {
       id: "perspiciatis",
     } satisfies ComponentProps<typeof Navbar>;
+    const nav = "voluptatem cumque accusantium";
+    const search = "totam molestiae tempora";
+    const settings = "dolore vero exercitationem";
     const result = await container.renderToString(Navbar, {
       props,
+      slots: { nav, search, settings },
     });
 
     expect.assertions(4);
 
     expect(result).toContain(props.id);
-    expect(result).toContain("Menu");
-    expect(result).toContain("Search");
-    expect(result).toContain("Settings");
+    expect(result).toContain(nav);
+    expect(result).toContain(search);
+    expect(result).toContain(settings);
+  });
+
+  it<LocalTestContext>("throws an error when the nav slot is missing", async ({
+    container,
+  }) => {
+    const props = {
+      id: "perspiciatis",
+    } satisfies ComponentProps<typeof Navbar>;
+    const search = "totam molestiae tempora";
+    const settings = "dolore vero exercitationem";
+
+    expect.assertions(1);
+
+    await expect(async () =>
+      container.renderToString(Navbar, {
+        props,
+        slots: { search, settings },
+      }),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: A nav slot is required.]`,
+    );
+  });
+
+  it<LocalTestContext>("throws an error when the search slot is missing", async ({
+    container,
+  }) => {
+    const props = {
+      id: "perspiciatis",
+    } satisfies ComponentProps<typeof Navbar>;
+    const nav = "voluptatem cumque accusantium";
+    const settings = "dolore vero exercitationem";
+
+    expect.assertions(1);
+
+    await expect(async () =>
+      container.renderToString(Navbar, {
+        props,
+        slots: { nav, settings },
+      }),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: A search slot is required.]`,
+    );
+  });
+
+  it<LocalTestContext>("throws an error when the settings slot is missing", async ({
+    container,
+  }) => {
+    const props = {
+      id: "perspiciatis",
+    } satisfies ComponentProps<typeof Navbar>;
+    const nav = "voluptatem cumque accusantium";
+    const search = "totam molestiae tempora";
+
+    expect.assertions(1);
+
+    await expect(async () =>
+      container.renderToString(Navbar, {
+        props,
+        slots: { nav, search },
+      }),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: A settings slot is required.]`,
+    );
   });
 });

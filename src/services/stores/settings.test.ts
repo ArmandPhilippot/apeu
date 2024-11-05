a479ae6b6aff7703d6cd157b1d145a4c5676fb56
Armand Philippot <git@armand.philippot.eu>
2024-11-05 14:30:54 +0100

    test: add missing tests for stores and themes

---
+++ src/services/stores/settings.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+import { isValidSettingsKey, type Settings } from "./settings";
+
+describe("is-valid-settings-key", () => {
+  it("returns true when the given string matches a setting key", () => {
+    const key = "theme" satisfies keyof Settings;
+    expect(isValidSettingsKey(key)).toBe(true);
+  });
+
+  it("returns false otherwise", () => {
+    expect(isValidSettingsKey("foo")).toBe(false);
+  });
+});
--- src/utils/themes.test.ts
+++ src/utils/themes.test.ts
@@ -1,5 +1,9 @@
 import { describe, expect, it } from "vitest";
-import { isValidTheme } from "./themes";
+import {
+  getPreferredColorScheme,
+  isValidTheme,
+  resolveCurrentColorScheme,
+} from "./themes";
 
 describe("is-valid-theme", () => {
   it("returns true when the value is `auto`", () => {
@@ -16,5 +20,25 @@ describe("is-valid-theme", () => {
 
   it("returns true when the value is invalid", () => {
     expect(isValidTheme("foo")).toBe(false);
+  });
+});
+
+describe("get-preferred-color-scheme", () => {
+  it("returns dark when prefers-color-scheme is not set to dark", () => {
+    expect(getPreferredColorScheme()).toBe("light");
+  });
+});
+
+describe("resolve-current-color-scheme", () => {
+  it("returns dark when the given theme is dark", () => {
+    expect(resolveCurrentColorScheme("dark")).toBe("dark");
+  });
+
+  it("returns light when the given theme is light", () => {
+    expect(resolveCurrentColorScheme("light")).toBe("light");
+  });
+
+  it("returns light by default when the given theme is set to auto", () => {
+    expect(resolveCurrentColorScheme("auto")).toBe("light");
   });
 });

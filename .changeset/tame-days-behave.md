b56c0834467759eb1db2de4a8fdd7dfa584a194d
Armand Philippot <git@armand.philippot.eu>
2025-01-10 18:23:43 +0100

    fix: handle correctly the contact form submissions (#53)
    * fix: make sure env vars are available with Docker
    * docs: add changeset

---
+++ .changeset/tame-days-behave.md
@@ -0,0 +1,5 @@
+---
+"apeu": patch
+---
+
+Fixes a contact form issue where environment variables was not available in a Docker container.
--- src/lib/nodemailer/index.ts
+++ src/lib/nodemailer/index.ts
@@ -7,15 +7,17 @@ import type { Options } from "nodemailer/lib/smtp-connection";
  * @returns {Options} The SMTP options
  */
 const getSMTPOptionsFromEnv = (): Options => {
+  const port = Number(process.env.SMTP_PORT);
+
   return {
-    host: import.meta.env.SMTP_HOST,
-    port: import.meta.env.SMTP_PORT,
+    host: process.env.SMTP_HOST,
+    port,
     /* The secure option should only be `true` with port `465`, so it should be
      * safe to set the option by checking the port */
-    secure: import.meta.env.SMTP_PORT === 465,
+    secure: port === 465,
     auth: {
-      user: import.meta.env.SMTP_USER,
-      pass: import.meta.env.SMTP_PASSWORD,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
     },
   };
 };
@@ -27,7 +29,11 @@ const getSMTPOptionsFromEnv = (): Options => {
  */
 export const createSMTPTransporter = (): Transporter => {
   const config = getSMTPOptionsFromEnv();
-  const transporter = nodemailer.createTransport(config);
+  const transporter = nodemailer.createTransport({
+    ...config,
+    connectionTimeout: 10000,
+    socketTimeout: 10000,
+  });
 
   return transporter;
 };

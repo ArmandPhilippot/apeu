71f727d65cf45277394cfad86f61af61159751e3
Armand Philippot <git@armand.philippot.eu>
2025-10-01 19:24:46 +0200

    refactor: replace Select with SelectField and improve its styles

--- src/components/atoms/select-field/select-field.test.ts
+++ src/components/atoms/select-field/select-field.test.ts
@@ -1,13 +1,13 @@
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import type { ComponentProps } from "astro/types";
 import { beforeEach, describe, expect, it } from "vitest";
-import Select from "./select.astro";
+import SelectField from "./select-field.astro";
 
 type LocalTestContext = {
   container: AstroContainer;
 };
 
-describe("Select", () => {
+describe("SelectField", () => {
   beforeEach<LocalTestContext>(async (context) => {
     context.container = await AstroContainer.create();
   });
@@ -16,47 +16,23 @@ describe("Select", () => {
     container,
   }) => {
     /* eslint-disable-next-line @typescript-eslint/no-magic-numbers -- Self-explanatory. */
-    expect.assertions(3);
+    expect.assertions(2);
 
     const props = {
-      label: "Choose an option",
       options: [
         { label: "Option 1", value: "option-1" },
         { label: "Option 2", value: "option-2" },
         { label: "Option 3", value: "option-3" },
       ],
       value: "option-2",
-    } satisfies ComponentProps<typeof Select>;
-    const result = await container.renderToString(Select, {
+    } satisfies ComponentProps<typeof SelectField>;
+    const result = await container.renderToString(SelectField, {
       props,
     });
     const optionsEl = [...result.matchAll(/<option.*?<\/option>/g)];
 
-    expect(result).toContain(props.label);
     expect(result).toContain("</select>");
     expect(optionsEl).toHaveLength(props.options.length);
-  });
-
-  it<LocalTestContext>("can visually hide its label", async ({ container }) => {
-    /* eslint-disable-next-line @typescript-eslint/no-magic-numbers -- Self-explanatory. */
-    expect.assertions(2);
-
-    const props = {
-      hideLabel: true,
-      label: "Choose an option",
-      options: [
-        { label: "Option 1", value: "option-1" },
-        { label: "Option 2", value: "option-2" },
-        { label: "Option 3", value: "option-3" },
-      ],
-      value: "option-2",
-    } satisfies ComponentProps<typeof Select>;
-    const result = await container.renderToString(Select, {
-      props,
-    });
-
-    expect(result).toContain(props.label);
-    expect(result).toContain("sr-only");
   });
 
   it<LocalTestContext>("throws an error if the value is invalid", async ({
@@ -65,20 +41,18 @@ describe("Select", () => {
     expect.assertions(1);
 
     const props = {
-      hideLabel: true,
-      label: "Choose an option",
       options: [
         { label: "Option 1", value: "option-1" },
         { label: "Option 2", value: "option-2" },
         { label: "Option 3", value: "option-3" },
       ],
       value: "option-2",
-    } satisfies ComponentProps<typeof Select>;
+    } satisfies ComponentProps<typeof SelectField>;
 
     const invalidProps = { ...props, value: "something" };
 
     await expect(async () =>
-      container.renderToString(Select, { props: invalidProps })
+      container.renderToString(SelectField, { props: invalidProps })
     ).rejects.toThrowErrorMatchingInlineSnapshot(
       `[Error: The provided value does not match any value in your "options". Received: something]`
     );

bb8cfbb399f02b20e5900308abdb2969fb793de7
Armand Philippot <git@armand.philippot.eu>
2025-10-19 21:43:20 +0200

    refactor: add back Img component

---
+++ src/components/atoms/img/img.test.ts
@@ -0,0 +1,47 @@
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { beforeEach, describe, expect, it } from "vitest";
+import Img from "./img.astro";
+
+type LocalTestContext = {
+  container: AstroContainer;
+};
+
+describe("Img", () => {
+  beforeEach<LocalTestContext>(async (context) => {
+    context.container = await AstroContainer.create();
+  });
+
+  it<LocalTestContext>("renders an image", async ({ container }) => {
+    /* eslint-disable-next-line @typescript-eslint/no-magic-numbers -- Self-explanatory. */
+    expect.assertions(3);
+
+    const alt = "voluptas repellendus nobis";
+    const src = "https://picsum.photos/640/480";
+    const result = await container.renderToString(Img, {
+      props: { alt, height: 480, src, width: 640 },
+    });
+
+    expect(result).toContain("<img");
+    expect(result).toContain(`alt="${alt}"`);
+    expect(result).toContain(`src="${src}"`);
+  });
+
+  it<LocalTestContext>("can wrap an image with a link", async ({
+    container,
+  }) => {
+    /* eslint-disable-next-line @typescript-eslint/no-magic-numbers -- Self-explanatory. */
+    expect.assertions(5);
+
+    const alt = "voluptas repellendus nobis";
+    const src = "https://picsum.photos/640/480";
+    const result = await container.renderToString(Img, {
+      props: { "data-clickable": "true", alt, height: 480, src, width: 640 },
+    });
+
+    expect(result).toContain("<a");
+    expect(result).toContain(`href="${src}"`);
+    expect(result).toContain("<img");
+    expect(result).toContain(`alt="${alt}"`);
+    expect(result).toContain(`src="${src}"`);
+  });
+});

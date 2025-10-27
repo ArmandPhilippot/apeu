c4b0b0b8923a09dabc292c3bdf650ffcb5c79940
Armand Philippot <git@armand.philippot.eu>
2025-10-28 00:22:13 +0100

    refactor: add back Breadcrumb component

---
+++ src/components/molecules/breadcrumb/breadcrumb.test.ts
@@ -0,0 +1,42 @@
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import type { ComponentProps } from "astro/types";
+import { beforeEach, describe, expect, it } from "vitest";
+import Breadcrumb from "./breadcrumb.astro";
+
+type LocalTestContext = {
+  container: AstroContainer;
+};
+
+describe("Breadcrumb", () => {
+  beforeEach<LocalTestContext>(async (context) => {
+    context.container = await AstroContainer.create();
+  });
+
+  it<LocalTestContext>("renders a list of breadcrumb items", async ({
+    container,
+  }) => {
+    /* eslint-disable-next-line @typescript-eslint/no-magic-numbers -- Self-explanatory. */
+    expect.assertions(5);
+
+    const props = {
+      items: [
+        { label: "omnis assumenda aut", path: "#voluptate-est-sed" },
+        { label: "voluptas odio voluptatem", path: "#est-ducimus-sit" },
+        { label: "sunt ratione quis", path: "#sunt-illo-iusto" },
+      ],
+    } satisfies ComponentProps<typeof Breadcrumb>;
+    const result = await container.renderToString(Breadcrumb, {
+      props,
+    });
+
+    const listItems = [...result.matchAll(/<li[^>]*>[\S\s]*?<\/li>/g)];
+
+    expect(listItems).toHaveLength(props.items.length);
+    expect(result).toContain(props.items[0]?.path);
+    expect(result).toContain(props.items[1]?.path);
+    /* eslint-disable-next-line @typescript-eslint/no-magic-numbers -- `path` index. */
+    expect(result).toContain(props.items[2]?.label);
+    /* eslint-disable-next-line @typescript-eslint/no-magic-numbers -- `path` index. */
+    expect(result).not.toContain(props.items[2]?.path);
+  });
+});

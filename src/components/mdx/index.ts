0dbf1e958f320c7187d4a87d0e0e2511d3459c16
Armand Philippot <git@armand.philippot.eu>
2025-11-01 22:21:34 +0100

    refactor: add back partial MDX components

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -1,1 +1,17 @@
-export const components = {};
+import Blockquote from "../atoms/blockquote/blockquote.astro";
+import Callout from "../atoms/callout/callout.astro";
+import Img from "../atoms/img/img.astro";
+import ListItem from "../atoms/list/list-item.astro";
+import List from "../atoms/list/list.astro";
+import Figure from "./figure.astro";
+import Ol from "./ol.astro";
+
+export const components = {
+  blockquote: Blockquote,
+  callout: Callout,
+  figure: Figure,
+  img: Img,
+  li: ListItem,
+  ol: Ol,
+  ul: List,
+};

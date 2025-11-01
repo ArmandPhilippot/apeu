75d214ab72ae7ff3856c32ed64b1d7bc7592644c
Armand Philippot <git@armand.philippot.eu>
2025-11-01 23:11:33 +0100

    refactor: add back Gallery and Placeholder MDX components

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -5,10 +5,12 @@ import ListItem from "../atoms/list/list-item.astro";
 import List from "../atoms/list/list.astro";
 import Figure from "./figure.astro";
 import Ol from "./ol.astro";
+import Placeholder from "./placeholder.astro";
 
 export const components = {
   blockquote: Blockquote,
   callout: Callout,
+  div: Placeholder,
   figure: Figure,
   img: Img,
   li: ListItem,

e0c24a792aa01b88610350706e5465c39401370f
Armand Philippot <git@armand.philippot.eu>
2025-11-01 22:59:20 +0100

    refactor: add back CardsList component

---
+++ src/components/molecules/cards-list/cards-list.test.ts
@@ -0,0 +1,98 @@
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import type { ComponentProps } from "astro/types";
+import { beforeEach, describe, expect, it } from "vitest";
+import CardsList from "./cards-list.astro";
+
+type LocalTestContext = {
+  container: AstroContainer;
+};
+
+describe("CardsList", () => {
+  beforeEach<LocalTestContext>(async (context) => {
+    context.container = await AstroContainer.create();
+  });
+
+  it<LocalTestContext>("renders a list of items", async ({ container }) => {
+    expect.assertions(1);
+
+    const props = {
+      items: [
+        {
+          body: "Inventore ex magnam sequi illo qui expedita reprehenderit. Sint optio magni accusamus. Omnis voluptas provident. Aut esse at enim qui molestias dolorem nihil.",
+          heading: "illum similique ut",
+          url: "#fuga-sunt-dicta",
+        },
+        {
+          body: "Nihil ad est doloribus. Aspernatur et est sit assumenda alias deleniti quia labore aut. Eaque itaque eius et error deserunt. Eos qui qui autem ut amet libero sit magni maxime. Illum laudantium repudiandae id et. Ut nostrum deleniti expedita velit.",
+          heading: "cumque id voluptatibus",
+          url: "#ex-corrupti-illum",
+        },
+      ],
+    } satisfies ComponentProps<typeof CardsList>;
+    const result = await container.renderToString(CardsList, {
+      props,
+      slots: { default: (item: (typeof props.items)[number]) => item.body },
+    });
+
+    const listItems = [...result.matchAll(/<li.*?<\/li>/g)];
+
+    expect(listItems).toHaveLength(props.items.length);
+  });
+
+  it<LocalTestContext>("can render a list of items defined as container", async ({
+    container,
+  }) => {
+    expect.assertions(1);
+
+    const props = {
+      isContainer: true,
+      items: [
+        {
+          body: "Inventore ex magnam sequi illo qui expedita reprehenderit. Sint optio magni accusamus. Omnis voluptas provident. Aut esse at enim qui molestias dolorem nihil.",
+          heading: "illum similique ut",
+          url: "#fuga-sunt-dicta",
+        },
+        {
+          body: "Nihil ad est doloribus. Aspernatur et est sit assumenda alias deleniti quia labore aut. Eaque itaque eius et error deserunt. Eos qui qui autem ut amet libero sit magni maxime. Illum laudantium repudiandae id et. Ut nostrum deleniti expedita velit.",
+          heading: "cumque id voluptatibus",
+          url: "#ex-corrupti-illum",
+        },
+      ],
+    } satisfies ComponentProps<typeof CardsList>;
+    const result = await container.renderToString(CardsList, {
+      props,
+      slots: { default: (item: (typeof props.items)[number]) => item.body },
+    });
+
+    expect(result).toContain("container");
+  });
+
+  it<LocalTestContext>("throws an error when the default slot is missing", async ({
+    container,
+  }) => {
+    expect.assertions(1);
+
+    const props = {
+      items: [
+        {
+          body: "Inventore ex magnam sequi illo qui expedita reprehenderit. Sint optio magni accusamus. Omnis voluptas provident. Aut esse at enim qui molestias dolorem nihil.",
+          heading: "illum similique ut",
+          url: "#fuga-sunt-dicta",
+        },
+        {
+          body: "Nihil ad est doloribus. Aspernatur et est sit assumenda alias deleniti quia labore aut. Eaque itaque eius et error deserunt. Eos qui qui autem ut amet libero sit magni maxime. Illum laudantium repudiandae id et. Ut nostrum deleniti expedita velit.",
+          heading: "cumque id voluptatibus",
+          url: "#ex-corrupti-illum",
+        },
+      ],
+    } satisfies ComponentProps<typeof CardsList>;
+
+    await expect(async () =>
+      container.renderToString(CardsList, {
+        props,
+      })
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: CardsList requires a function that describes how to render a single item as child.]`
+    );
+  });
+});

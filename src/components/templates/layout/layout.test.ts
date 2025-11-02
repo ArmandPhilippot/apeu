9a61524ae92cd9295e3ddcb8c9160eebef0a5c8a
Armand Philippot <git@armand.philippot.eu>
2025-11-02 13:58:13 +0100

    refactor: add back JSON-LD graphs

--- src/components/templates/layout/layout.test.ts
+++ src/components/templates/layout/layout.test.ts
@@ -120,4 +120,57 @@ describe("Layout", () => {
 
     expect(result).toContain(body);
   });
+
+  it<LocalTestContext>("can render a link to the design system in dev mode", async ({
+    container,
+  }) => {
+    expect.assertions(1);
+
+    vi.stubEnv("MODE", "development");
+
+    const props = {
+      seo: {
+        title: "est et fugiat",
+      },
+    } satisfies ComponentProps<typeof Layout>;
+    const result = await container.renderToString(Layout, {
+      props,
+    });
+
+    expect(result).toContain("/design-system");
+  });
+
+  it<LocalTestContext>("does not render a link to the design system in other modes than dev", async ({
+    container,
+  }) => {
+    expect.assertions(1);
+
+    vi.stubEnv("MODE", "production");
+    vi.stubEnv("DEV", false);
+
+    const props = {
+      seo: {
+        title: "est et fugiat",
+      },
+    } satisfies ComponentProps<typeof Layout>;
+    const result = await container.renderToString(Layout, {
+      props,
+    });
+
+    expect(result).not.toContain("/design-system");
+  });
+
+  it<LocalTestContext>("renders the JSON-LD graphs", async ({ container }) => {
+    /* eslint-disable-next-line @typescript-eslint/no-magic-numbers -- Self-explanatory. */
+    expect.assertions(2);
+
+    const props = {
+      graphs: [{ "@type": "WebPage" }],
+      seo: { title: "est et fugiat" },
+    } satisfies ComponentProps<typeof Layout>;
+    const result = await container.renderToString(Layout, { props });
+
+    expect(result).toContain("application/ld+json");
+    expect(result).toContain("WebPage");
+  });
 });

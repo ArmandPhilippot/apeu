c8ba03fabccb47019195b88e6484aa3271c96525
Armand Philippot <git@armand.philippot.eu>
2025-11-03 23:16:27 +0100

    fix: add missing types

--- src/types/data.ts
+++ src/types/data.ts
@@ -15,8 +15,19 @@ export type Route = {
   path: string;
 };
 
+export type Icon = { name: IconName; size?: number };
+
 export type WithIcon<T> = T & {
-  icon?: { name: IconName; size?: number } | null | undefined;
+  icon?: Icon | null | undefined;
+};
+
+type MetaValue = string | Date | Route;
+
+export type MetaItem = {
+  label: string;
+  values: MetaValue[];
+  icon?: { name: IconName; size?: number | string } | undefined | null;
+  description?: string;
 };
 
 export type AltLanguage = {

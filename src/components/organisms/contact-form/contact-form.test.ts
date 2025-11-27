4da2820df9b11ae5425530dbd775af7c15cc49e3
Armand Philippot <git@armand.philippot.eu>
2025-11-27 23:15:11 +0100

    refactor: add back ContactForm component

---
+++ src/components/organisms/contact-form/contact-form.test.ts
@@ -0,0 +1,35 @@
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import type { ComponentProps } from "astro/types";
+import { beforeEach, describe, expect, it } from "vitest";
+import { useI18n } from "../../../services/i18n";
+import { CONFIG } from "../../../utils/constants";
+import ContactForm from "./contact-form.astro";
+
+type LocalTestContext = {
+  container: AstroContainer;
+};
+
+describe("ContactForm", () => {
+  beforeEach<LocalTestContext>(async (context) => {
+    context.container = await AstroContainer.create();
+  });
+
+  it<LocalTestContext>("renders a contact form", async ({ container }) => {
+    /* eslint-disable-next-line @typescript-eslint/no-magic-numbers -- Self-explanatory. */
+    expect.assertions(5);
+
+    const { translate } = useI18n(CONFIG.LANGUAGES.DEFAULT);
+    const props = {
+      id: "odit",
+    } satisfies ComponentProps<typeof ContactForm>;
+    const result = await container.renderToString(ContactForm, {
+      props,
+    });
+
+    expect(result).toContain("</form>");
+    expect(result).toContain(translate("form.contact.label.name"));
+    expect(result).toContain(translate("form.contact.label.email"));
+    expect(result).toContain(translate("form.contact.label.object"));
+    expect(result).toContain(translate("form.contact.label.message"));
+  });
+});

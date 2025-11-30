86403be6e5984127c9b8547eec77fd6b8d29f09f
Armand Philippot <git@armand.philippot.eu>
2025-12-01 00:15:22 +0100

    refactor: add back Link component for MDX contents

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -4,10 +4,12 @@ import Img from "../atoms/img/img.astro";
 import ListItem from "../atoms/list/list-item.astro";
 import List from "../atoms/list/list.astro";
 import Figure from "./figure.astro";
+import Link from "./link.astro";
 import Ol from "./ol.astro";
 import Placeholder from "./placeholder.astro";
 
 export const components = {
+  a: Link,
   blockquote: Blockquote,
   callout: Callout,
   div: Placeholder,

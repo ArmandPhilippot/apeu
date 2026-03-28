c13cfd0d78b21620350015f8626af32a9364d5b1
Armand Philippot <git@armand.philippot.eu>
2026-03-28 14:17:47 +0100

    fix: new caught lint error, no unecessary type parameters

--- src/lib/nodemailer/index.ts
+++ src/lib/nodemailer/index.ts
@@ -12,25 +12,23 @@ import type SMTPTransport from "nodemailer/lib/smtp-transport";
  *
  * @returns {Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options>} The NodeMailer transporter.
  */
-export const createSMTPTransporter = (): Transporter<
-  SMTPTransport.SentMessageInfo,
-  SMTPTransport.Options
-> => {
-  const SECURE_PORT = 465;
+export const createSMTPTransporter =
+  (): Transporter<SMTPTransport.SentMessageInfo> => {
+    const SECURE_PORT = 465;
 
-  const transporter = createTransport({
-    host: SMTP_HOST,
-    port: SMTP_PORT,
-    /* The secure option should only be `true` with port `465`, so it should be
-     * safe to set the option by checking the port */
-    secure: SMTP_PORT === SECURE_PORT,
-    auth: {
-      user: SMTP_USER,
-      pass: SMTP_PASSWORD,
-    },
-    connectionTimeout: 10_000,
-    socketTimeout: 10_000,
-  });
+    const transporter = createTransport({
+      host: SMTP_HOST,
+      port: SMTP_PORT,
+      /* The secure option should only be `true` with port `465`, so it should be
+       * safe to set the option by checking the port */
+      secure: SMTP_PORT === SECURE_PORT,
+      auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASSWORD,
+      },
+      connectionTimeout: 10_000,
+      socketTimeout: 10_000,
+    });
 
-  return transporter;
-};
+    return transporter;
+  };

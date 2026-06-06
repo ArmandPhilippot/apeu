a5aac82363cff508d28ca2e9a177bea1d4d29327
Armand Philippot <git@armand.philippot.eu>
2026-06-06 14:33:23 +0200

    feat: switch to Sätteri as Markdown processor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 // @ts-check
+import { satteri } from "@astrojs/markdown-satteri";
 import mdx from "@astrojs/mdx";
 import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig, envField, fontProviders } from "astro/config";
 import icon from "astro-icon";
-import remarkDirective from "remark-directive";
 import { astroStories } from "./src/lib/astro/integrations/astro-stories";
 import { devOnlyPages } from "./src/lib/astro/integrations/dev-only-pages";
 import { pagefind } from "./src/lib/astro/integrations/pagefind";
-import { rehypeCodeBlocks } from "./src/lib/rehype/rehype-code-blocks";
-import { rehypeDisableExplicitJsx } from "./src/lib/rehype/rehype-disable-explicit-jsx";
-import { rehypeImages } from "./src/lib/rehype/rehype-images";
-import { remarkCallouts } from "./src/lib/remark/remark-callouts";
-import { remarkWordsCount } from "./src/lib/remark/remark-words-count";
 import { shikiTheme } from "./src/lib/shiki/theme";
 import { CONFIG } from "./src/utils/constants";
 
@@ -135,8 +130,9 @@ export default defineConfig({
     }),
   ],
   markdown: {
-    remarkPlugins: [remarkDirective, remarkCallouts, remarkWordsCount],
-    rehypePlugins: [rehypeDisableExplicitJsx, rehypeCodeBlocks, rehypeImages],
+    processor: satteri({
+      features: { directive: true },
+    }),
     shikiConfig: {
       theme: shikiTheme,
     },
